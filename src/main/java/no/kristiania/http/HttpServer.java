0b00f54afd431bce96f9d110d9e685dce925a73e
venici2606 <56083213+venici2606@users.noreply.github.com>
2020-09-28 15:22:39 +0200

    Dev (#1)
    * server should return content-length (failing) #4
    
    * server should return content-length #4
    
    * server should return response body #4
    
    * server should return response body (first was failing) now green #4
    
    * server should return file from disk #4
    
    * server should return file from disk #4
    
    * server should return correct content type (failing) #4
    
    * server should return correct content type #4
    
    * server should return 404 is file not found (failing) #4
    
    * server should return 404 is file not found. Resolves #4

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -1,10 +1,14 @@
 package no.kristiania.http;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpServer {
+
+    private File contentRoot; // property
 
     public HttpServer(int port) throws IOException {
         //forteller vi java at hver gang en klient connecte seg, så skal de komme til vårt program
@@ -32,30 +36,79 @@ public class HttpServer {
 
         String requestTarget = requestLine.split(" ")[1]; //splitter request line
         String statusCode = "200";
+        String body = "Hello <strong>World</strong>!";
 
         int questionPos = requestTarget.indexOf('?');
+
+        // String requestPath = questionPos != -1 ? requestTarget.substring(0, questionPos) : requestTarget;
+        String requestPath;
+        if (questionPos != -1) { //hvis vi ikke fant reqtarget
+            requestPath = requestTarget.substring(0, questionPos);
+        } else {
+            requestPath = requestTarget;
+        }
+
         if (questionPos != -1) {
 
             QueryString queryString = new QueryString(requestTarget.substring(questionPos+1));
+            if (queryString.getParameter("status") != null) {
+                /*    int equalPos = queryString.indexOf('=');
+                String parameterName = queryString.substring(0, equalPos);
+                String parameterValue = queryString.substring(equalPos+1);  */
+                statusCode = queryString.getParameter("status");
+            }
+            if(queryString.getParameter("body") != null) {
+                body = queryString.getParameter("body");
+            }
+        } else if (!requestPath.equals("/echo")){ //hvis den ikke finner ? så skal den lete i disk
+            File file = new File(contentRoot, requestPath);
 
-        /*    int equalPos = queryString.indexOf('=');
-            String parameterName = queryString.substring(0, equalPos);
-            String parameterValue = queryString.substring(equalPos+1);  */
-            statusCode = queryString.getParameter("status");
+            if(!file.exists()) {
+                body =  file + " does not exist";
+                String response = "HTTP/1.1 404 Not Found\r\n" +
+                        "Content-Length: " + body.length() + "\r\n" +
+                        "\r\n" +
+                        body;
+
+                clientSocket.getOutputStream().write(response.getBytes());
+                return;
+            }
+
+            statusCode = "200";
+            String contentType = "text/plain"; //default
+
+            if(file.getName().endsWith(".html")){
+                contentType = "text/html";
+            }
+            String response = "HTTP/1.1 " + statusCode + " OK\r\n" +
+                    "Content-Length: " + file.length() + "\r\n" +
+                    "Content-Type: " + contentType + "\r\n" +
+                    "\r\n";
+
+            //skrive 200, lengden på filen, og skrive ut dette pog overføre til klienten
+            clientSocket.getOutputStream().write(response.getBytes());
+
+            new FileInputStream(file).transferTo(clientSocket.getOutputStream());
+
         }
 
         String response = "HTTP/1.1 " + statusCode + " OK\r\n" +
-                "Content-Length: 29\r\n" +
+                "Content-Length: " + body.length() + "\r\n" +
                 "Content-Type: text/plain\r\n" +
                 "\r\n" +
-                "Hello <strong>World</strong>!";
+                body;
 
         clientSocket.getOutputStream().write(response.getBytes());
     }
 
     public static void main(String[] args) throws IOException {
 
-        new HttpServer(8080);
+        HttpServer server = new HttpServer(8080);
+        server.setContentRoot(new File("src/main/resources")); //katalogen, der i java prosjekt der settes inn filer som ikke er java fil
 
+    }
+
+    public void setContentRoot(File contentRoot) {
+        this.contentRoot = contentRoot;
     }
 }
